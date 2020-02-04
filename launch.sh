184ee055d30e0f9b29cae660ee9a5fa617ea6975
Marius Damien <marius.damien@gmail.com>
2020-02-04 04:03:38 +0100

    [DEVOPS] - Ajout d'un script d'install pour l'image raspberry

---
+++ launch.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+sudo apt-get install libffi-dev libssl-dev
+sudo apt-get install -y python python-pip
+sudo apt-get remove python-configparser
+sudo pip install docker-compose
